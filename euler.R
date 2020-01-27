0b8831ded8ab0037cf05ae9e08ec29b35c4816a1
erkancalaf <erkancalaf@outlook.com>
2020-01-27 21:35:43 +0100

    Update euler.R

--- euler.R
+++ euler.R
@@ -9,6 +9,7 @@ euler_1 <- function(x){
   return(j)
 }
 
+euler_1(1000)
 
 # 2. euler_2 ------------------------------
 euler_2 <- function(x){
@@ -26,3 +27,23 @@ euler_2 <- function(x){
   retVal <- sum(temp[temp<x & temp %% 2 != 0])
   return(retVal)
 }
+
+sum(euler_2(4000000))
+
+# 3. euler_3 ------------------------------
+euler_3 <- function(x){
+  result_factor <- c()
+  i = 2
+  while (x!=1) {
+    while (x %% i == 0) {
+      x = x/i
+      result_factor <- c(result_factor,i)
+    }
+    i = i+1
+  }
+  return(result_factor)
+}
+
+max(euler_3(600851475143 ))
+
+
