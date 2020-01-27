191229e43ccc933068640d0c22ba5ba9e433a96c
erkancalaf <erkancalaf@outlook.com>
2020-01-27 23:06:13 +0100

    Update euler_1.R

--- euler_1.R
+++ euler_1.R
@@ -1,4 +1,5 @@
-# 1. euler_1 ------------------------------
+# Euler_1 Solution ------------------------------
+# https://projecteuler.net/problem=1
 euler_1 <- function(x){
   j = 0
   for (i in 1:x-1) {
